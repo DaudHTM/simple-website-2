dc5027718c356b481312383c6d2153d5e48ab397
DaudHTM <DaudHTM@gmail.com>
2022-08-09 05:25:31 -0400

    added my name intro and i am animation

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+
+var am = ['Youtuber', 'Student','Web Dev','Roboticist','Body Builder'];
+let x=0;
+
+
+const textAm=document.getElementById('pos')
+const menu = document.querySelector('.open')       
+const close= document.querySelector('.close')
+const nav=document.querySelector('nav')
+menu.addEventListener('click', () =>{
+    nav.classList.add('open-nav')
+
+})
+close.addEventListener('click', () =>{
+    nav.classList.remove('open-nav')
+
+})
+
+setInterval(change, 2000);
+
+function change(){
+    textAm.classList.add('fade');
+
+    setTimeout(function() {
+
+        
+        textAm.innerHTML = am[x];
+        textAm.classList.remove('fade');
+        console.log(am[x]);
+        x++;
+        if(x>4)
+        {
+            x=0;
+        }
+    },500);
+}
